be317a3727ddb873684a1300bba40453f361e771
Hervé <h.richard.codeur.onlinem>
2021-04-28 15:29:34 +0200

    cards

---
+++ pages/index.php
@@ -0,0 +1,44 @@
+<?php include "../includes/header.php" ?>
+
+<div class="container">
+  <div class="row align-items-start">
+    <div class="col">
+        <div class="card bg-dark text-white">
+            <img src="..." class="card-img" alt="...">
+            <div class="card-img-overlay">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text">Last updated 3 mins ago</p>
+            </div>
+        </div> 
+    </div>
+    <div class="col">
+        <div class="card bg-dark text-white">
+            <img src="../assets/images/pictures/community-manager.jpg" class="card-img" alt="...">
+            <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start">
+                <h5 class="card-title">Community Manager</h5>
+            </div>
+            <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight mb-3">
+                <button class="mt-auto p-2 bd-highlight btn btn-warning text-light">Plus d' infos</button>
+            </div>
+            
+        </div> 
+    </div>
+    <div class="col">
+        <div class="card bg-dark text-white">
+            <img src="..." class="card-img" alt="...">
+            <div class="card-img-overlay">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text">Last updated 3 mins ago</p>
+            </div>
+        </div> 
+    </div>
+  </div>
+</div>
+
+
+
+
+
+<?php include "../includes/footer.php" ?>
