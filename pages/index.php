3ba4f46636d324b4e31cc6aa9c7e720f5f71d36b
Hervé <h.richard.codeur.onlinem>
2021-05-03 14:57:14 +0200

    update integration

--- pages/index.php
+++ pages/index.php
@@ -3,12 +3,13 @@
 <div class="">
   <div class="d-flex justify-content-start help">
     <div class="col">
-        <div class="card bg-dark text-white">
-            <img src="..." class="card-img" alt="...">
-            <div class="card-img-overlay">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text">Last updated 3 mins ago</p>
+    <div class="card bg-dark text-white w-100">
+            <img src="../assets/images/pictures/community-manager.jpg" class="card-img" alt="...">
+            <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start">
+                <h5 class="card-title">Community Manager</h5>
+            </div>
+            <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight">
+                <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
             </div>
         </div> 
     </div>
@@ -22,19 +23,24 @@
                 <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
             </div>
             
+            
         </div> 
     </div>
     <div class="col">
-        <div class="card bg-dark text-white">
-            <img src="..." class="card-img" alt="...">
-            <div class="card-img-overlay">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text">Last updated 3 mins ago</p>
+    <div class="card bg-dark text-white w-100">
+            <img src="../assets/images/pictures/community-manager.jpg" class="card-img" alt="...">
+            <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start">
+                <h5 class="card-title">Community Manager</h5>
+            </div>
+            <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight">
+                <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
             </div>
         </div> 
+
     </div>
+
   </div>
+  
 </div>
 
 
