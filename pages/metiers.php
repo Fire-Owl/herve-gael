d088f2f28dcf7f8ce57293203b899065113b9da3
Hervé <h.richard.codeur.onlinem>
2021-05-04 11:14:01 +0200

    responsive-card

--- pages/metiers.php
+++ pages/metiers.php
@@ -1,113 +1,68 @@
 <?php include "../includes/header.php" ?>
 
-<div class="col w-75">
-  <div class="d-flex justify-content-end help">
-    <div class="col">
-    <div class="card bg-dark text-white w-75">
-            <img src="../assets/images/pictures/community-manager.jpg" class="card-img" alt="...">
-            <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start">
-                <h5 class="card-title">Community Manager</h5>
-            </div>
-            <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight">
-                <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
-            </div>
-        </div> 
-    </div>
-    <div class="col">
-        <div class="card bg-dark text-white w-75">
-            <img src="../assets/images/pictures/community-manager.jpg" class="card-img" alt="...">
-            <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start">
-                <h5 class="card-title">Community Manager</h5>
-            </div>
-            <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight">
-                <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
-            </div>
-            
-            
-        </div> 
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-md-3 col-sm-12 pb-5">
+            <div class="card bg-dark text-white no-margin">
+                <img src="../assets/images/pictures/community-manager.jpg" class="card-img" alt="...">
+                <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start">
+                    <h5 class="card-title">Community Manager</h5>
+                </div>
+                <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight">
+                    <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
+                </div>
+            </div> 
+        </div>
+        <div class="col-md-3 col-sm-12 pb-5">
+            <div class="card bg-dark text-white no-margin">
+                <img src="../assets/images/pictures/community-manager.jpg" class="card-img" alt="...">
+                <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start">
+                    <h5 class="card-title">Community Manager</h5>
+                </div>
+                <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight">
+                    <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
+                </div>
+            </div> 
+        </div>
+        <div class="col-md-3 col-sm-12 pb-5">
+            <div class="card bg-dark text-white no-margin flex-nowrap">
+                <img src="../assets/images/pictures/community-manager.jpg" class="card-img" alt="...">
+                <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start">
+                    <h5 class="card-title">Community Manager</h5>
+                </div>
+                <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight">
+                    <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
+                </div>
+            </div> 
+        </div>
+        <div class="col-md-3 col-sm-12 pb-5">
+            <div class="card bg-dark text-white flex-nowrap no-margin">
+                <img src="../assets/images/pictures/community-manager.jpg" class="card-img" alt="...">
+                <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start">
+                    <h5 class="card-title">Community Manager</h5>
+                </div>
+                <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight">
+                    <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
+                </div>
+            </div> 
+        </div>
     </div>
 
-    <div class="col">
-        <div class="card bg-dark text-white w-75">
-            <img src="../assets/images/pictures/community-manager.jpg" class="card-img" alt="...">
-            <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start">
-                <h5 class="card-title">Community Manager</h5>
-            </div>
-            <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight">
-                <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
-            </div>
-            
-            
-        </div> 
-    </div>
-    <div class="col">
-    <div class="card bg-dark text-white w-75">
-            <img src="../assets/images/pictures/community-manager.jpg" class="card-img" alt="...">
-            <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start">
-                <h5 class="card-title">Community Manager</h5>
-            </div>
-            <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight">
-                <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
-            </div>
-        </div> 
-
-    </div>
-
-  </div>
-  <div class="d-flex justify-content-start help">
-    <div class="col">
-    <div class="card bg-dark text-white w-75">
-            <img src="../assets/images/pictures/community-manager.jpg" class="card-img" alt="...">
-            <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start">
-                <h5 class="card-title">Community Manager</h5>
-            </div>
-            <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight">
-                <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
-            </div>
-        </div> 
-    </div>
-    <div class="col">
-        <div class="card bg-dark text-white w-75">
-            <img src="../assets/images/pictures/community-manager.jpg" class="card-img" alt="...">
-            <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start">
-                <h5 class="card-title">Community Manager</h5>
-            </div>
-            <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight">
-                <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
-            </div>
-            
-            
-        </div> 
-    </div>
-
-    <div class="col">
-        <div class="card bg-dark text-white w-75">
-            <img src="../assets/images/pictures/community-manager.jpg" class="card-img" alt="...">
-            <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start">
-                <h5 class="card-title">Community Manager</h5>
-            </div>
-            <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight">
-                <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
-            </div>
-            
-            
-        </div> 
-    </div>
-    <div class="col">
-    <div class="card bg-dark text-white w-75">
-            <img src="../assets/images/pictures/community-manager.jpg" class="card-img" alt="...">
-            <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start">
-                <h5 class="card-title">Community Manager</h5>
-            </div>
-            <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight">
-                <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
-            </div>
-        </div> 
-
-    </div>
-
-  </div>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
