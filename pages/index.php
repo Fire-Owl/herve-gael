79837f6c786a06ab0740fedb074a61b6122169a0
Hervé <h.richard.codeur.onlinem>
2021-05-04 17:18:45 +0200

    update

--- pages/index.php
+++ pages/index.php
@@ -1,20 +1,10 @@
 <?php include "../includes/header.php" ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
-    <title>Home</title>
-</head>
-<body>
     
 <div class="container">
     <div class="row">
 
-        <div class="col">
+        <div class="col-12 col-md-6 p-3">
             <p class="text-introduction">Bienvenue sur le site des métiers du web</p>
             <p class="text-introduction">Un projet réalisé par Gaël et Hervé dans le cadre de leur formation <br />
             Access Code School</p>
@@ -25,19 +15,18 @@
         </div>
 
 
-        <div class="col">
+        <div class="col-12 col-md-6 p-3">
 
-        <div class="col-md-3 col-6 pb-3">
-            <div class="card bg-dark text-white no-margin img-card-landing">
-                <img src="../assets/images/pictures/pentester.jpg" class="card-img img-fluid" alt="...">
-                <div class="card-img-overlay d-flex flex-column bd-highlight justify-content-start cardlanding-title-background">
-                    <h5 class="card-title">Pentester</h5>
-                </div>
-                <div class="card-img-overlay d-flex align-items-end flex-column bd-highlight">
-                    <button class="mt-auto p-2 bd-highlight btn btn-primary shadow-none px-3 me-1">Plus d' infos</button>
-                </div>
-            </div> 
-        </div>
+        <div class="home__container-card">
+
+            <img src="../assets/images/pictures/pentester.jpg" alt="Notebook" style="width:100%;">
+
+            <div class="home__container-card--content">
+                <h1>Pentester</h1>
+                <p>Un métier cool</p>
+            </div>
+
+            </div>
 
         </div>
 
@@ -55,9 +44,6 @@
 
 
 
-
-</body>
-</html>
 
 
 
