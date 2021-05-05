371ca5eb6f9b3f42b787b583c7ed6bbd31ca4056
Hervé <h.richard.codeur.onlinem>
2021-05-05 10:52:50 +0200

    articles analyste finish

--- pages/analyste-test.php
+++ pages/analyste-test.php
@@ -4,13 +4,47 @@ include "../includes/header.php"; ?>
 <div class="container d-flex justify-content-around">
   <div class="row">
     <div class="col-sm">
-
-
-
+    <h1 id="l-analyste-test-et-validation">L&#39;analyste test et validation</h1><br />
+<p>L&#39;analyste test et validation, aussi appellé ingénieur test ou testeur, a pour activité principale la validation des logiciels ou applications avant leur déploiement.</p>
+<h2 id="ses-missions">Ses missions</h2>
+<ul>
+<li>Prendre connaissance des spécifications du produit, de ses modules, des exigences des utilisateur et les analyser</li>
+<li>Concevoir des tests basés sur les analyses des points cités précédement, en rédiger les procédures</li>
+<li>Mettre en place les outils de tests</li>
+<li>Exécuter les tests et les corriger jusqu&#39;à validation des spécifications</li>
+<li>Consigner les résultats des tests, rédiger un rapport de synthèse</li>
+<li>Réaliser un suivi des performances via un monitoring quotidien</li>
+</ul>
+<h2 id="ses-comp-tences">Ses compétences</h2>
+<ul>
+<li>Maitrise des outils de test ( exemple : ALM )</li>
+<li>Comprendre l&#39;aspect fonctionnel de l&#39;application testée</li>
+<li>L&#39;utilisation de langue de programmation fonctionnelle et de script (ex : Python)</li>
+<li>Connaitre et appliquer les méthodologies de tests commes celle de l&#39;ISTQB (Comité international de qualification du test logiciel)</li>
+</ul>
+<h2 id="possibilit-s-d-volution">Possibilités d&#39;évolution</h2>
+<ul>
+<li>Consultant en processus de test</li>
+<li>Chef de projet test</li>
+<li>Chargé de méthode en outils et qualité</li>
+</ul>
+<h2 id="salaire">Salaire</h2>
+<ul>
+<li>Pour un débutant, la moyenne du salaire annuel se situera entre 33.000€ et 37.000€ </li>
+</ul>
 
     </div>
     <div class="col-sm">
-      IMG + TEXT
+    <img src="../assets/images/pictures/Analyste-test.jpg" class="card-img-articles" alt="...">
+    <h2 id="-tudes-et-formations">Études et formations</h2>
+<ul>
+<li>Spécialisation en école d&#39;Ingénieur</li>
+<li>BTS, DEUST, DUT, Licence Pro</li>
+<li><p>CNAM &quot;Analyst Testing&quot;</p>
+</li>
+<li><p>Certifications : CFTL / ISTQB</p>
+</li>
+</ul>
     </div>
   </div>
 </div>
