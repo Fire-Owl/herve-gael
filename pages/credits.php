a8577188637cbe9319b3ef7100ece19632f0e5b2
Hervé <h.richard.codeur.onlinem>
2021-05-05 13:27:24 +0200

    update mentions & credits

---
+++ pages/credits.php
@@ -0,0 +1,12 @@
+<?php $current ='metiers';
+include "../includes/header.php"; ?>
+
+
+
+<p>les images utilisés sont tirées de Pixabay.com</p>
+
+
+
+
+
+<?php include "../includes/footer.php" ?>
--- pages/mentions-legales.php
+++ pages/mentions-legales.php
@@ -2,22 +2,13 @@
 include "../includes/header.php"; ?>
 
 
-
+<h2>Les mentions légales</h2><br />
 <h3>Conditions d’utilisation</h3>
 <p>L’utilisation du présent site implique l’acceptation pleine et entière des conditions générales d’utilisation décrites ci-après. Ces conditions d’utilisation sont susceptibles d’être modifiées ou complétées à tout moment.
 Informations<br />
 Les informations et documents du site sont présentés à titre indicatif, n’ont pas de caractère exhaustif, et ne peuvent engager la responsabilité du propriétaire du site. Le propriétaire du site ne peut être tenu responsable des dommages directs et indirects consécutifs à l’accès au site.<p>
 
-<h3>Interactivité</h3>
-<p>Les utilisateurs du site peuvent y déposer du contenu, apparaissant sur le site dans des espaces dédiés (notamment via les commentaires). Le contenu déposé reste sous la responsabilité de leurs auteurs, qui en assument pleinement l’entière responsabilité juridique.<p>
-<p>Le propriétaire du site se réserve néanmoins le droit de retirer sans préavis et sans justification tout contenu déposé par les utilisateurs qui ne satisferait pas à la charte déontologique du site ou à la législation en vigueur.</p>
-<h3>Propriété intellectuelle</h3>
-<p>Sauf mention contraire, tous les éléments accessibles sur le site (textes, images, graphismes, logo, icônes, sons, logiciels, etc.) restent la propriété exclusive de leurs auteurs, en ce qui concerne les droits de propriété intellectuelle ou les droits d’usage.</p>
-<p>Toute reproduction, représentation modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de l’auteur.23
-Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient est considérée comme constitutive d’une contrefaçon et poursuivie. 4
-Les marques et logos reproduits sur le site sont déposés par les sociétés qui en sont propriétaires.</p>
-
-<h3>Liens Liens sortants</h3>
+<h3>Liens sortants</h3>
 <p>Le propriétaire du site décline toute responsabilité et n’est pas engagé par le référencement via des liens hypertextes, de ressources tierces présentes sur le réseau Internet, tant en ce qui concerne leur contenu que leur pertinence.</p>
 <h3>Liens entrants</h3>
 <p>Le propriétaire du site autorise les liens hypertextes vers l’une des pages de ce site, à condition que ceux-ci ouvrent une nouvelle fenêtre et soient présentés de manière non équivoque afin d’éviter : tout risque de confusion entre le site citant et le propriétaire du site, ainsi que toute présentation tendancieuse, ou contraire aux lois en vigueur.</p>
@@ -37,6 +28,8 @@ Mentions légales – Version 1.5 – Utilisation libre sous Licence Creative Co
 8.Loi n° 2004-801 du 6 août 2004</br >
 9.Article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique
 </p>
+
+
 
 
 
