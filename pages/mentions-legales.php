0da73dab4d4b31ab58748a479e3caf2dfd25af9a
Hervé <h.richard.codeur.onlinem>
2021-05-05 13:08:31 +0200

    update mentions legales

--- pages/mentions-legales.php
+++ pages/mentions-legales.php
@@ -8,7 +8,7 @@ include "../includes/header.php"; ?>
 Informations<br />
 Les informations et documents du site sont présentés à titre indicatif, n’ont pas de caractère exhaustif, et ne peuvent engager la responsabilité du propriétaire du site. Le propriétaire du site ne peut être tenu responsable des dommages directs et indirects consécutifs à l’accès au site.<p>
 
-<h3>Interactivité<h3>
+<h3>Interactivité</h3>
 <p>Les utilisateurs du site peuvent y déposer du contenu, apparaissant sur le site dans des espaces dédiés (notamment via les commentaires). Le contenu déposé reste sous la responsabilité de leurs auteurs, qui en assument pleinement l’entière responsabilité juridique.<p>
 <p>Le propriétaire du site se réserve néanmoins le droit de retirer sans préavis et sans justification tout contenu déposé par les utilisateurs qui ne satisferait pas à la charte déontologique du site ou à la législation en vigueur.</p>
 <h3>Propriété intellectuelle</h3>
@@ -26,15 +26,15 @@ Les marques et logos reproduits sur le site sont déposés par les sociétés qu
 <p>Tout utilisateur dispose d’un droit d’accès, de rectification et d’opposition aux données personnelles le concernant, en effectuant sa demande écrite et signée, accompagnée d’une preuve d’identité. 5678 Le site ne recueille pas d’informations personnelles, et n’est pas assujetti à déclaration à la CNIL.</p>
 
 <p>Crédits < crédits photo, icones, etc. >
-Mentions légales – Version 1.5 – Utilisation libre sous Licence Creative Commons CC BY-NC-ND 3.0 FR / creativecommons.org
-1.Articles L111-1 et suivants du Code de la Propriété Intellectuelle du 1er juillet 1992
-2.Article 41 de la loi du 11 mars 1957
-3.Article L. 226-13 du Code pénal et la Directive Européenne du 24 octobre 1995
-4.Articles L.335-2 et suivants du Code de Propriété Intellectuelle
-5.Loi n° 78-87 du 6 janvier 1978, modifiée par la loi n° 2004-801 du 6 août 2004, relative à l’informatique, aux fichiers et aux libertés
-6.Articles 38 et suivants de la loi 78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés
-7.Loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données
-8.Loi n° 2004-801 du 6 août 2004
+Mentions légales – Version 1.5 – Utilisation libre sous Licence Creative Commons CC BY-NC-ND 3.0 FR / creativecommons.org</p>
+<p>1.Articles L111-1 et suivants du Code de la Propriété Intellectuelle du 1er juillet 1992</br >
+2.Article 41 de la loi du 11 mars 1957</br >
+3.Article L. 226-13 du Code pénal et la Directive Européenne du 24 octobre 1995</br >
+4.Articles L.335-2 et suivants du Code de Propriété Intellectuelle</br >
+5.Loi n° 78-87 du 6 janvier 1978, modifiée par la loi n° 2004-801 du 6 août 2004, relative à l’informatique, aux fichiers et aux libertés</br >
+6.Articles 38 et suivants de la loi 78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés</br >
+7.Loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données</br >
+8.Loi n° 2004-801 du 6 août 2004</br >
 9.Article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique
 </p>
 
